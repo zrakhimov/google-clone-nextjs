074d37560834ebd968b82810afb3689cab958b39
zokir.rakhimov@gmail.com <zokir.rakhimov@gmail.com>
2022-07-16 21:52:48 -0400

    Get data from Google API and create mock data

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
+import response from "../response";
 
-export default function search() {
+export default function search({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -13,4 +15,24 @@ export default function search() {
       {/* Search Results */}
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // change mockData to false if you want actual api call
+  const mockData = true;
+  const data = mockData
+    ? response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+          context.query.searchType && "&searchType=image"
+        }`
+      ).then((res) => res.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
---
+++ response.js
@@ -0,0 +1,272 @@
+const data = {
+  kind: "customsearch#search",
+  url: {
+    type: "application/json",
+    template:
+      "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+  },
+  queries: {
+    request: [
+      {
+        title: "Google Custom Search - wikipedia",
+        totalResults: "100200000000",
+        searchTerms: "wikipedia",
+        count: 10,
+        startIndex: 1,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "65c58465bb3a3b2f5",
+        searchType: "image",
+      },
+    ],
+    nextPage: [
+      {
+        title: "Google Custom Search - wikipedia",
+        totalResults: "100200000000",
+        searchTerms: "wikipedia",
+        count: 10,
+        startIndex: 11,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "65c58465bb3a3b2f5",
+        searchType: "image",
+      },
+    ],
+  },
+  context: {
+    title: "go-v3",
+  },
+  searchInformation: {
+    searchTime: 0.137775,
+    formattedSearchTime: "0.14",
+    totalResults: "100200000000",
+    formattedTotalResults: "100,200,000,000",
+  },
+  items: [
+    {
+      kind: "customsearch#result",
+      title: "Wikipedia - Wikipedia",
+      htmlTitle: "<b>Wikipedia</b> - <b>Wikipedia</b>",
+      link: "https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png",
+      displayLink: "en.wikipedia.org",
+      snippet: "Wikipedia - Wikipedia",
+      htmlSnippet: "<b>Wikipedia</b> - <b>Wikipedia</b>",
+      mime: "image/png",
+      fileFormat: "image/png",
+      image: {
+        contextLink: "https://en.wikipedia.org/wiki/Wikipedia",
+        height: 1095,
+        width: 1200,
+        byteSize: 446341,
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHI6liiUl9Dk_TrmPWFs2ZTeU9OqiRWdYEYZzkGc0HXmpvrEJLaj5pROwo&s",
+        thumbnailHeight: 137,
+        thumbnailWidth: 150,
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "What Is Wikipedia? Here's What You Should Know",
+      htmlTitle: "What Is <b>Wikipedia</b>? Here&#39;s What You Should Know",
+      link: "https://i.insider.com/5fbd515550e71a001155724f?width=700",
+      displayLink: "www.businessinsider.com",
+      snippet: "What Is Wikipedia? Here's What You Should Know",
+      htmlSnippet: "What Is <b>Wikipedia</b>? Here&#39;s What You Should Know",
+      mime: "image/",
+      fileFormat: "image/",
+      image: {
+        contextLink: "https://www.businessinsider.com/what-is-wikipedia",
+        height: 525,
+        width: 700,
+        byteSize: 45511,
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiGNe9PG7GbHQlurPet-6Lt6yVIesbL7zSnBZneTclUD1w0SL1tpqSOeQ&s",
+        thumbnailHeight: 105,
+        thumbnailWidth: 140,
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "English Wikipedia - Wikipedia",
+      htmlTitle: "English <b>Wikipedia</b> - <b>Wikipedia</b>",
+      link: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Wikipedia-logo-v2-en.svg/1200px-Wikipedia-logo-v2-en.svg.png",
+      displayLink: "en.wikipedia.org",
+      snippet: "English Wikipedia - Wikipedia",
+      htmlSnippet: "English <b>Wikipedia</b> - <b>Wikipedia</b>",
+      mime: "image/png",
+      fileFormat: "image/png",
+      image: {
+        contextLink: "https://en.wikipedia.org/wiki/English_Wikipedia",
+        height: 1378,
+        width: 1200,
+        byteSize: 366712,
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3U7AIzxzDdzjghYOtjeqourNQlTNo2XPYPolCkPl3_EUqQ595pO7WI-o&s",
+        thumbnailHeight: 150,
+        thumbnailWidth: 131,
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Russia threatens to block access to Wikipedia – EURACTIV.com",
+      htmlTitle:
+        "Russia threatens to block access to <b>Wikipedia</b> – EURACTIV.com",
+      link: "https://www.euractiv.com/wp-content/uploads/sites/2/2022/03/w_56617973.jpg",
+      displayLink: "www.euractiv.com",
+      snippet: "Russia threatens to block access to Wikipedia – EURACTIV.com",
+      htmlSnippet:
+        "Russia threatens to block access to <b>Wikipedia</b> – EURACTIV.com",
+      mime: "image/jpeg",
+      fileFormat: "image/jpeg",
+      image: {
+        contextLink:
+          "https://www.euractiv.com/section/media/news/russia-threatens-to-block-access-to-wikipedia/",
+        height: 613,
+        width: 920,
+        byteSize: 81375,
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBHSvmkVrGN9x0cfBI1T4js_qCKqK9Zglr1n-Q0lrdNxyu0Dcr5cFqUBI&s",
+        thumbnailHeight: 98,
+        thumbnailWidth: 147,
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "German Wikipedia - Wikipedia",
+      htmlTitle: "German <b>Wikipedia</b> - <b>Wikipedia</b>",
+      link: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wikipedia-logo-v2-de.svg/1200px-Wikipedia-logo-v2-de.svg.png",
+      displayLink: "en.wikipedia.org",
+      snippet: "German Wikipedia - Wikipedia",
+      htmlSnippet: "German <b>Wikipedia</b> - <b>Wikipedia</b>",
+      mime: "image/png",
+      fileFormat: "image/png",
+      image: {
+        contextLink: "https://en.wikipedia.org/wiki/German_Wikipedia",
+        height: 1378,
+        width: 1200,
+        byteSize: 368474,
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4vxlsRfvx3xdHVH43ENdy5m9oGlmL0gyiW5_v4NlzwZv3yiNvFIH_Zpc&s",
+        thumbnailHeight: 150,
+        thumbnailWidth: 131,
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Wikipedia – Writing in the Margins",
+      htmlTitle: "<b>Wikipedia</b> – Writing in the Margins",
+      link: "https://blogs.stthomas.edu/english/files/2016/04/Wikipedia-Logo.jpg",
+      displayLink: "blogs.stthomas.edu",
+      snippet: "Wikipedia – Writing in the Margins",
+      htmlSnippet: "<b>Wikipedia</b> – Writing in the Margins",
+      mime: "image/jpeg",
+      fileFormat: "image/jpeg",
+      image: {
+        contextLink: "https://blogs.stthomas.edu/english/tag/wikipedia/",
+        height: 1080,
+        width: 1920,
+        byteSize: 124333,
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlAW-4Loc2oXrmtvyXklP_xu4StWfl1XuMVasuvBep0Vc9vn0yKA9Swmo&s",
+        thumbnailHeight: 84,
+        thumbnailWidth: 150,
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Indonesian Wikipedia - Wikipedia",
+      htmlTitle: "Indonesian <b>Wikipedia</b> - <b>Wikipedia</b>",
+      link: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Wikipedia-logo-v2-id.svg/1200px-Wikipedia-logo-v2-id.svg.png",
+      displayLink: "en.wikipedia.org",
+      snippet: "Indonesian Wikipedia - Wikipedia",
+      htmlSnippet: "Indonesian <b>Wikipedia</b> - <b>Wikipedia</b>",
+      mime: "image/png",
+      fileFormat: "image/png",
+      image: {
+        contextLink: "https://en.wikipedia.org/wiki/Indonesian_Wikipedia",
+        height: 1378,
+        width: 1200,
+        byteSize: 368270,
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIA9hdxc4zQOxVOPCRVtt_voMaRYXXkGHYnSVwJLW2Ujt2Ezq68vtBc3w&s",
+        thumbnailHeight: 150,
+        thumbnailWidth: 131,
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title:
+        "Meta, Wikipedia want to fight misinformation with new AI tool Sphere",
+      htmlTitle:
+        "Meta, <b>Wikipedia</b> want to fight misinformation with new AI tool Sphere",
+      link: "https://image.cnbcfm.com/api/v1/image/104771436-GettyImages-527461708.jpg?v=1532563745&w=1600&h=900",
+      displayLink: "www.cnbc.com",
+      snippet:
+        "Meta, Wikipedia want to fight misinformation with new AI tool Sphere",
+      htmlSnippet:
+        "Meta, <b>Wikipedia</b> want to fight misinformation with new AI tool Sphere",
+      mime: "image/jpeg",
+      fileFormat: "image/jpeg",
+      image: {
+        contextLink:
+          "https://www.cnbc.com/2022/07/13/meta-wikipedia-want-to-fight-misinformation-with-new-ai-tool-sphere.html",
+        height: 900,
+        width: 1600,
+        byteSize: 135678,
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj_RonqvjF60RB08yaEvFZrlYYrJT017pdaZ7D0Ig00SqnyXH6FtZ1F2E&s",
+        thumbnailHeight: 84,
+        thumbnailWidth: 150,
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Swedish Wikipedia - Wikipedia",
+      htmlTitle: "Swedish <b>Wikipedia</b> - <b>Wikipedia</b>",
+      link: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Wikipedia-logo-v2-sv.svg/1200px-Wikipedia-logo-v2-sv.svg.png",
+      displayLink: "en.wikipedia.org",
+      snippet: "Swedish Wikipedia - Wikipedia",
+      htmlSnippet: "Swedish <b>Wikipedia</b> - <b>Wikipedia</b>",
+      mime: "image/png",
+      fileFormat: "image/png",
+      image: {
+        contextLink: "https://en.wikipedia.org/wiki/Swedish_Wikipedia",
+        height: 1378,
+        width: 1200,
+        byteSize: 381033,
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ29EBTJ11h7zNlrfYhIO6AC8OV4cFlqu6nSIu84WDhQG3DpFgHP7-4K6F-&s",
+        thumbnailHeight: 150,
+        thumbnailWidth: 131,
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Amazon.com: The Game About Wikipedia : Toys & Games",
+      htmlTitle:
+        "Amazon.com: The Game About <b>Wikipedia</b> : Toys &amp; Games",
+      link: "https://m.media-amazon.com/images/I/61YYkAO+zfL._AC_SL1500_.jpg",
+      displayLink: "www.amazon.com",
+      snippet: "Amazon.com: The Game About Wikipedia : Toys & Games",
+      htmlSnippet:
+        "Amazon.com: The Game About <b>Wikipedia</b> : Toys &amp; Games",
+      mime: "image/jpeg",
+      fileFormat: "image/jpeg",
+      image: {
+        contextLink:
+          "https://www.amazon.com/Cardinal-The-Game-About-Wikipedia/dp/B07TBWF5XJ",
+        height: 1095,
+        width: 1185,
+        byteSize: 74826,
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo1LexQx7mcbttZNbRdb3QP5DZsEcL-8TP0cyCjnl3EYWAJ_qTHp1e0CQQ&s",
+        thumbnailHeight: 139,
+        thumbnailWidth: 150,
+      },
+    },
+  ],
+};
+
+export default data;
